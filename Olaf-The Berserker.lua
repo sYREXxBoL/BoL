a85c6d65b83d670bf481ff9e0643f90ea1f18736
sYREXxBoL <tim.kobe@t-online.de>
2016-02-07 18:10:33 +0100

    Update Olaf-The Berserker.lua

--- Olaf-The Berserker.lua
+++ Olaf-The Berserker.lua
@@ -1,4 +1,4 @@
-version = "1.04"
+version = "1.05"
 
 
 --[[
@@ -43,7 +43,7 @@ end
 		OlafMenu = scriptConfig("Olaf - The Berserker", "Olaf")
 		
 		OlafMenu:addSubMenu("Combo Settings", "combo")
-			OlafMenu.combo:addParam("comboKey", "Full Combo Key (SBTW)", SCRIPT_PARAM_ONKEYDOWN, false, string.byte (" "))
+			OlafMenu.combo:addParam("comboKey", "Full Combo Key (SBTW)", SCRIPT_PARAM_ONKEYDOWN, false, string.byte (" ")) 
 			OlafMenu.combo:addParam("ultimatec", "Use Auto-Ultimate in Combo", SCRIPT_PARAM_ONOFF, true)
 
 		
@@ -66,6 +66,7 @@ end
 			OlafMenu.lane:addParam("laneQ", "Use (Q)", SCRIPT_PARAM_ONOFF, true)
 			OlafMenu.lane:addParam("laneW", "Use (W)", SCRIPT_PARAM_ONOFF, true)
 			OlafMenu.lane:addParam("laneE", "USe (E)", SCRIPT_PARAM_ONOFF, true)
+			OlafMenu.lane:addParam("laneMana", "Min. Mana Percent: ", SCRIPT_PARAM_SLICE, 50, 0, 100, 0)
 
 			
 		OlafMenu:addSubMenu("Jungle Clear Settings", "jungle")
@@ -73,6 +74,7 @@ end
 			OlafMenu.jungle:addParam("jungleQ", "Use (Q)", SCRIPT_PARAM_ONOFF, true)
 			OlafMenu.jungle:addParam("jungleW", "Use (W)", SCRIPT_PARAM_ONOFF, true)
 			OlafMenu.jungle:addParam("jungleE", "USe (E)", SCRIPT_PARAM_ONOFF, true)
+			OlafMenu.jungle:addParam("jungleMana", "Min. Mana Percent: ", SCRIPT_PARAM_SLICE, 50, 0, 100, 0)
 		
 		OlafMenu:addSubMenu("Flee Settings", "flee")
 			OlafMenu.flee:addParam("fleeKey", "Flee Key (G)", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("G"))
@@ -81,6 +83,10 @@ end
 		OlafMenu:addSubMenu("KillSteal Settings", "ks")
 			OlafMenu.ks:addParam("killSteal", "Use Smart Kill Steal", SCRIPT_PARAM_ONOFF, true)
 			OlafMenu.ks:addParam("autoIgnite", "Auto Ignite", SCRIPT_PARAM_ONOFF, true)
+
+		OlafMenu:addSubMenu("Misc Settings", "misc")
+			OlafMenu.misc:addParam("autopickAxe", "Auto Pick Axe", SCRIPT_PARAM_ONOFF, true)
+			OlafMenu.misc:addParam("autopickAxerange", "Auto Pick Axe Range", SCRIPT_PARAM_SLICE, 365, 0, 1000)
 				
 
 		OlafMenu:addSubMenu("Draw Settings", "drawing")	
@@ -88,6 +94,7 @@ end
 			OlafMenu.drawing:addParam("Target", "Draw Circle on Target", SCRIPT_PARAM_ONOFF, true)
 			OlafMenu.drawing:addParam("qDraw", "Draw (Q) Range", SCRIPT_PARAM_ONOFF, true)
 			OlafMenu.drawing:addParam("eDraw", "Draw (E) Range", SCRIPT_PARAM_ONOFF, true)
+			OlafMenu.drawing:addParam("axeDraw", "Draw Axe", SCRIPT_PARAM_ONOFF, true)
 
 
 		OlafMenu:addParam("OlafVer", "Version: ", SCRIPT_PARAM_INFO, version)		
@@ -106,7 +113,14 @@ end
 
 		        if (OlafMenu.drawing.eDraw) and eReady then
 		            DrawCircle3DE(myHero.x, myHero.y, myHero.z)
-		        end     
+		        end
+
+		        if (OlafMenu.drawing.axeDraw) then
+		        	if axepos ~= nil then
+		        		DrawCircle3DAxe(axepos.x, axepos.y, axepos.z)
+		        		DrawLine3DAxe(axepos.x, axepos.y, axepos.z, myHero.x, myHero.y, myHero.z)
+		        	end
+		        end
 			end
 		end
 	end
@@ -134,10 +148,10 @@ end
         eReady = myHero:CanUseSpell(_E) == READY
         rReady = myHero:CanUseSpell(_R) == READY
 
-        SpellQ = {name = "Spear Shot",			range =  600, ready = false, dmg = 0, manaUsage = 0				   }
-		SpellW = {name = "Aegis of Zeonia",		range =  600, ready = false, dmg = 0, manaUsage = 0				   }
-		SpellE = {name = "Heartseeker Strike",	range =  700, ready = false, dmg = 0, manaUsage = 0				   }
-		SpellR = {name = "Grand Skyfall",		range = 5500, ready = false, dmg = 0, manaUsage = 0				   }
+        SpellQ = {name = "Spear Shot",			range =  1000	, ready = false, dmg = 0, manaUsage = 0				   }
+		SpellW = {name = "Aegis of Zeonia",		range =  125	, ready = false, dmg = 0, manaUsage = 0				   }
+		SpellE = {name = "Heartseeker Strike",	range =  325	, ready = false, dmg = 0, manaUsage = 0				   }
+		SpellR = {name = "Grand Skyfall",		range = 	0	, ready = false, dmg = 0, manaUsage = 0				   }
 
 
         SpellQ.manaUsage = myHero:GetSpellData(_Q).mana
@@ -170,6 +184,15 @@ end
 		if FleeKey then
 			Flee()
 		end
+
+		if OlafMenu.misc.autopickAxe and axepos and not FleeKey and not qReady and GetDistance(myHero, axepos) <= (OlafMenu.misc.autopickAxerange) then
+            pickingaxe = true
+            myHero:MoveTo(tonumber(axepos.x), tonumber(axepos.z))
+        end
+
+        if OlafMenu.misc.autopickAxe and axepos and not FleeKey and not qReady and GetDistance(myHero, axepos) > (OlafMenu.misc.autopickAxerange) then
+            pickingaxe = false
+        end
 
 		if OlafMenu.harass.autoQ then
 			HerassAutoQ()
@@ -212,9 +235,9 @@ end
 		end
 	end
 
-        function ImCC()
-        if HaveBuff(myHero, 3) then return true
-        elseif HaveBuff(myHero, 5) then return true
+    function ImCC()
+    	if HaveBuff(myHero, 3) then return true
+    	elseif HaveBuff(myHero, 5) then return true
         elseif HaveBuff(myHero, 8) then return true
         elseif HaveBuff(myHero, 10) then return true
         elseif HaveBuff(myHero, 11) then return true
@@ -226,7 +249,7 @@ end
         else
         return false
         end
-        end
+    end
 
 	function HerassAutoQ()
 
@@ -289,7 +312,7 @@ end
 
             if minions and ValidTarget(minions) then
 
-				if GetDistance(minions) <= 1000 and OlafMenu.lane.laneQ then
+				if GetDistance(minions) <= 1000 and OlafMenu.lane.laneQ and not isLow('Mana', myHero, OlafMenu.lane.laneMana) then
 					FarmQ(minions)
 				end
 
@@ -316,7 +339,7 @@ end
 					CastSpell(_W, jm)
 				end
 
-				if GetDistance(jm) <= 1000 and OlafMenu.jungle.jungleQ then
+				if GetDistance(jm) <= 1000 and OlafMenu.jungle.jungleQ and not isLow('Mana', myHero, OlafMenu.jungle.jungleMana) then
 					CastQ(jm)
 				end
 
@@ -382,6 +405,27 @@ end
                     end
                 DrawLines2(points, width or 1, color or 2294967295)
     end
+
+    function DrawCircle3DAxe(x, y, z, radius, width, color, quality)
+                radius = radius or 125
+                quality = quality and 3 * math.pi / quality or 3 * math.pi / (radius / 3)
+                local points = {}
+                    for theta = 0, 2 * math.pi + quality, quality do
+                        local c = WorldToScreen(D3DXVECTOR3(x + radius * math.cos(theta), y, z - radius * math.sin(theta)))
+                        points[#points + 1] = D3DXVECTOR2(c.x, c.y)
+                    end
+                DrawLines2(points, width or 3, color or 4294967280)
+    end
+
+    function DrawLine3DAxe(x1, y1, z1, x2, y2, z2, width, color)
+    	local p = WorldToScreen(D3DXVECTOR3(x1, y1, z1))
+    	local px, py = p.x, p.y
+    	local c = WorldToScreen(D3DXVECTOR3(x2, y2, z2))
+    	local cx, cy = c.x, c.y
+    	if OnScreen({ x = px, y = py }, { x = px, y = py }) then
+        	DrawLine(cx, cy, px, py, width or 3, color or 4294967295)
+    	end
+	end
 
 
 	function autoIgnite()
@@ -474,4 +518,21 @@ end
 				return false
 			end
 		end
+	end
+
+
+	function OnCreateObj(object)
+
+	    if myHero.dead then return end
+	    if object ~= nil and object.name ~= nil and object.name == "olaf_axe_totem_team_id_green.troy" and object.x ~= nil and object.z ~= nil then
+	        axepos = object
+	    end
+	end
+
+
+	function OnDeleteObj(object)
+	    if object ~= nil and object.name ~= nil and object.name == "olaf_axe_totem_team_id_green.troy" and object.x ~= nil and object.z ~= nil then
+	        axepos = nil
+	        pickingaxe = false
+	    end
 	end
